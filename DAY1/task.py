457e66be816705beadc490d46c22e2a0d4cca730
Sanya Kansal <124577767+sannya07@users.noreply.github.com>
2024-10-03 23:22:38 +0530

    Add files via upload

---
+++ DAY1/task.py
@@ -0,0 +1,25 @@
+print("hello world\nhi")
+# \n means new line
+
+print("hello"+" "+"Angela")
+# string concatination
+
+# python causes indentation errors as well
+
+print("Hello "+ input("what is your name?") + "!")
+# input is done using input()
+
+# variables dont need to be defined by data types
+name= input("what is your last name?")
+
+# in java there is name.length()
+print(len(name))
+
+# in java we can take $ signs in variable names as well, in python its not the case
+
+ 
+
+
+
+
+
